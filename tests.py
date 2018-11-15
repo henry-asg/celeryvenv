46a1748eaccb60167b22cde1a723c23989837142
Henry <Henry.Wang@asggroup.com.au>
2018-11-15 13:23:32 +1100

    add test_2

--- tests.py
+++ tests.py
@@ -10,5 +10,8 @@ class TestCeleryMethods(unittest.TestCase):
     def test_n1_1(self):
         self.assertEqual(run_tasks.func_run_tasks(), 1)
 
+    def test_n1_2(self):
+        self.assertEqual(run_tasks.func_run_tasks(), 2)
+
 if __name__ == '__main__':
     unittest.main()
