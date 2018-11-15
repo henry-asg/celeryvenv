45988fdafa8334099f5f1d4961b2adeb8422e2d2
Henry <Henry.Wang@asggroup.com.au>
2018-11-15 14:39:52 +1100

    comment out test234

--- tests.py
+++ tests.py
@@ -7,14 +7,14 @@ class TestCeleryMethods(unittest.TestCase):
     def test_n1(self):
         self.assertEqual(run_tasks.func_run_tasks(), 0)
 
-    def test_n1_1(self):
-        self.assertEqual(run_tasks.func_run_tasks(), 1)
+    # def test_n1_1(self):
+    #     self.assertEqual(run_tasks.func_run_tasks(), 1)
 
-    def test_n1_2(self):
-        self.assertEqual(run_tasks.func_run_tasks(), 2)
+    # def test_n1_2(self):
+    #     self.assertEqual(run_tasks.func_run_tasks(), 2)
 
-    def test_n1_3(self):
-        self.assertEqual(run_tasks.func_run_tasks(), 3)
+    # def test_n1_3(self):
+    #     self.assertEqual(run_tasks.func_run_tasks(), 3)
 
 if __name__ == '__main__':
     import xmlrunner
