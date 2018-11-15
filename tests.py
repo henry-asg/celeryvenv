d20024ddcaec1155049f2b9f78cce8b8ac82b715
Henry <Henry.Wang@asggroup.com.au>
2018-11-15 13:13:13 +1100

    assert return 1

--- tests.py
+++ tests.py
@@ -7,5 +7,8 @@ class TestCeleryMethods(unittest.TestCase):
     def test_n1(self):
         self.assertEqual(run_tasks.func_run_tasks(), 0)
 
+    def test_n1_1(self):
+        self.assertEqual(run_tasks.func_run_tasks(), 1)
+
 if __name__ == '__main__':
     unittest.main()
